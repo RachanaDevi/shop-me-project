db7ed1923a1f5d531e5a6dbbd1653822bea5fe6e
Rachana Devi <rachanadevi18@gmail.com>
2023-08-09 01:23:36 +0530

    Rachana | Add User Controller and Service to get Manage Users page

---
+++ ShopMeWebParent/ShopMeBackend/src/main/java/com/shopme/admin/user/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.shopme.admin.user.controller;
+
+import com.shopme.admin.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String listAll() {
+        return "users";
+    }
+}
